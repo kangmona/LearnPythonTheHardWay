5ffc8bc7eb32fab1d634311364c1d27b055039a4
kangmona <kangmona@users.noreply.github.com>
2016-01-09 17:36:42 +0800

    Update ex13-2.py

--- ex13-2.py
+++ ex13-2.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 # argv和raw_input()不同点：用户输入的时机。
-# 如果参数是在用户执行命令时就要输入，那就是argv，见ex13.py
+# 如果参数是在用户执行命令时就要输入，那就是argv，见ex13-0.py
 # 如果参数是在脚本运行过程中需要用户输入，那就用raw_input()，见本ex13-1.py
 # 如果要结合argv和raw_input()，见ex13-2.py
 from sys import argv
